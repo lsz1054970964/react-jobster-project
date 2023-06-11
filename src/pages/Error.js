95266cc12bf3bbf9fcaac97a39a546f7f37e79ba
lsz1054970964 <lsz1054970964@gmail.com>
2023-06-11 14:07:50 +0200

    finish error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import notFound from "../assets/images/not-found.svg";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={notFound} alt="not-found"></img>
+        <h3>Ohh! Page Not Found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/landing">Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
